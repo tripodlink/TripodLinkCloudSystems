1ae1ede80771e16b5bb1c08a4df6be95e29fec19
LorenzVergara <LorenzVergara@LAPTOP-J3MVNN1C>
2020-03-11 11:02:33 +0800

    Add Item Group In Data Dictionary

---
+++ CloudIMS/DATA DICTIONARY/DIC_ITMGRP_ItemGroup/Areas/INV/ViewModels/ItemGroupViewModel.cs
@@ -0,0 +1,23 @@
+﻿using CloudImsCommon.Extensions;
+using CloudImsCommon.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataDictionary.Unit.ViewModels
+{
+    public class ItemGroupViewModel : AppViewModel
+    {
+        public ItemGroupViewModel(HttpContext httpcontext)
+            :base(httpcontext)
+        {
+
+        }
+
+        public List<ProgramMenu> ProgramMenus { get; set; } = new List<ProgramMenu>();
+
+        public List<ItemGroup> ItemGroups { get; set; } = new List<ItemGroup>();
+    }
+}
