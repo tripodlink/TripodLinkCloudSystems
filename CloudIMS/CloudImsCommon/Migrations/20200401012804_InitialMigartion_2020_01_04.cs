6e0ba71e7ecda848d45b74682bf73e751c66d69f
Reymart Villanueva <reymart.e.villanueva@gmail.com>
2020-04-01 10:21:28 +0800

    update project

---
+++ CloudIMS/CloudImsCommon/Migrations/20200401012804_InitialMigartion_2020_01_04.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CloudImsCommon.Migrations
+{
+    public partial class InitialMigartion_2020_01_04 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "user_account",
+                keyColumn: "ua_user_id",
+                keyValue: "SYSAD",
+                columns: new[] { "im_created_on", "im_updated_on" },
+                values: new object[] { new DateTime(2020, 4, 1, 9, 28, 3, 715, DateTimeKind.Local), new DateTime(2020, 4, 1, 9, 28, 3, 716, DateTimeKind.Local) });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "user_account",
+                keyColumn: "ua_user_id",
+                keyValue: "SYSAD",
+                columns: new[] { "im_created_on", "im_updated_on" },
+                values: new object[] { new DateTime(2020, 4, 1, 9, 24, 56, 783, DateTimeKind.Local), new DateTime(2020, 4, 1, 9, 24, 56, 784, DateTimeKind.Local) });
+        }
+    }
+}
