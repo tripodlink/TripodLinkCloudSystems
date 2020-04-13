66accc60637d4126a38b75fbe66c5ff15a259116
Reymart Villanueva <reymart.e.villanueva@gmail.com>
2020-04-13 11:09:47 +0800

    Add two tables InventoryIn trxHeader and Detail

---
+++ CloudIMS/CloudImsCommon/Models/InventoryInTrxDetail.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CloudImsCommon.Models
+{
+    [Table("inventoryin_trx_detail")]
+    public class InventoryInTrxDetail
+    {
+        [Key]
+        [Required]
+        [Column("trxno")]
+        [MaxLength(100, ErrorMessage = "TransactionNo cannot exceed 10 characters.")]
+        public String TransactionNo { get; set; }
+
+        [Required]
+        [Column("item_id")]
+        [MaxLength(100, ErrorMessage = "Item ID cannot exceed 100 characters.")]
+        public String ItemID { get; set; }
+
+        [Required]    
+        [Column("unit")]    
+
+        [MaxLength(100, ErrorMessage = "Unit cannot exceed 100 characters.")]
+        public String Unit { get; set; }
+
+
+        [Required]
+        [Column("quantity")]
+        [MaxLength(100, ErrorMessage = "Quantity cannot exceed 100 characters.")]
+        public int Quantity { get; set; }
+
+        [Required]
+        [Column("lotno")]
+        [MaxLength(100, ErrorMessage = "LotNumber cannot exceed 100 characters.")]
+        public int LotNumber { get; set; }
+
+        [Required]
+        [Column("exp_date")]
+        [MaxLength(100, ErrorMessage = "ExpirationDate cannot exceed 100 characters.")]
+        public DateTime ExpirationDate { get; set; }
+
+        [Required]
+        [Column("count")]
+        [MaxLength(100, ErrorMessage = "Count cannot exceed 100 characters.")]
+        public int Count { get; set; }
+
+        [Required]
+        [Column("remaning_count")]
+        [MaxLength(100, ErrorMessage = "RemainigCount cannot exceed 100 characters.")]
+        public int RemainigCount { get; set; }
+
+    }
+}
---
+++ CloudIMS/CloudImsCommon/Models/InventoryInTrxHeader.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CloudImsCommon.Models
+{
+    [Table("inventoryin_trx_header")]
+    public class InventoryInTrxHeader
+    {
+        [Key]
+        [Required]
+        [Column("trxno")]
+        [MaxLength(100, ErrorMessage = "TransactionNo cannot exceed 100 characters.")]
+        public String TransactionNo { get; set; }
+
+        [Required]
+        [Column("trx_date")]
+        [MaxLength(100, ErrorMessage = "TransactionDate cannot exceed 100 characters.")]
+        public DateTime TransactionDate { get; set; }
+
+        [Required]
+        [Column("rcvd_date")]
+        [MaxLength(100, ErrorMessage = "ReceivedDate cannot exceed 100 characters.")]
+        public DateTime ReceivedDate { get; set; }
+
+
+        [Required]
+        [Column("rcvd_by")]
+        [MaxLength(100, ErrorMessage = "ReceivedBy cannot exceed 100 characters.")]
+        public String ReceivedBy { get; set; }
+
+        [Required]
+        [Column("po_number")]
+        [MaxLength(100, ErrorMessage = "PONumber cannot exceed 100 characters.")]
+        public String PONumber { get; set; }
+
+        [Required]
+        [Column("invoice_number")]
+        [MaxLength(100, ErrorMessage = "InvoiceNo cannot exceed 100 characters.")]
+        public String InvoiceNo { get; set; }
+
+        [Required]
+        [Column("ref_number")]
+        [MaxLength(100, ErrorMessage = "ReferenceNo cannot exceed 100 characters.")]
+        public String ReferenceNo { get; set; }
+
+        [Required]
+        [Column("doc_number")]
+        [MaxLength(100, ErrorMessage = "DocumnetNo cannot exceed 100 characters.")]
+        public String DocumnetNo { get; set; }
+
+        [Required]
+        [Column("supplier")]
+        [MaxLength(100, ErrorMessage = "Supplier cannot exceed 100 characters.")]
+        public String Supplier { get; set; }
+            
+        [Required]
+        [Column("remarks")]
+        [MaxLength(100, ErrorMessage = "Remarks cannot exceed 100 characters.")]
+        public String Remarks { get; set; }
+
+
+
+    }
+}
