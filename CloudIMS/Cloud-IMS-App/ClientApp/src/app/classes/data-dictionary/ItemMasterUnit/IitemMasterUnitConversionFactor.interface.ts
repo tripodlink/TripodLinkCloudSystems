57ef02f2aece73d5ddc372b6a2efd75f2ea00ac8
Reymart Villanueva <reymart.e.villanueva@gmail.com>
2020-04-17 15:34:23 +0800

    Update InventoryIn Screen

---
+++ CloudIMS/Cloud-IMS-App/ClientApp/src/app/classes/data-dictionary/ItemMasterUnit/IitemMasterUnitConversionFactor.interface.ts
@@ -0,0 +1,11 @@
+
+export interface IitemMasterUnitConversionFactor {
+  id: string;
+  itemMasterUnitUnit: string;
+  itemMasterUnitConversion: string;
+  itemMasterUnitMinimumVal: string;
+
+  code: string;
+  description: string;
+  shortDescription: string;
+}
